51dd2ed60ddb5ce28ec14c499d3d211ee9fa9909
strongpu <strongpu@tencent.com>
2021-02-27 17:02:45 +0800

    fix

--- hello.py
+++ hello.py
@@ -1,5 +1,13 @@
+import random
+
+
 def hello():
-    print ('' is not None)
+    print('' is not None)
+
+
+def getRandom():
+    return random.randint(1, 100)
+
 
 if __name__ == "__main__":
     hello()
